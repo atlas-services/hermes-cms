b8476128d7d8defe38c757eae5c72eef4c5d9a99
Tayeb-Chikhi-at-659326164628 <tayeb.chikhi@ceres-advisory.io>
2026-05-30 21:39:36 +0200

    deploy sans d:s:u

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,6 @@ done
 composer install --no-dev --optimize-autoloader
 composer run deploy-assets
 
-symfony console d:s:u --force
 symfony console c:c && chmod -Rf 777 var/cache/ var/log/ var/sessions/ data/db/ public/bundles public/uploads
 
 echo "Déploiement terminé."
